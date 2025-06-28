8e0272ad9c66ee1c720c6ad29c1211b74eac2d7d
Neeraga Nagaraj <neerava.nraj@gmail.com>
2025-06-28 19:33:59 +0530

    Update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,6 +5,63 @@ lsp.ensure_installed({
     'rust_analyzer'
 })
 
+local lspconfig = require("lspconfig")
+
+vim.g.autoformat = false
+vim.g.zig_fmt_autosave = 0
+
+lspconfig.ts_ls.setup({})
+
+lspconfig.rust_analyzer.setup({
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+            checkOnSave = { command = "clippy" },
+            diagnostics = {
+                disabled = { "unlinked-file" }
+            }
+        }
+    }
+})
+
+lspconfig.lua_ls.setup {
+    on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc')) then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend("force", capabilities, require('cmp_nvim_lsp').default_capabilities())
+lspconfig.gdscript.setup(capabilities)
+
 lsp.on_attach(function (client, bufnr)
     local opts =  { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end, opts)
