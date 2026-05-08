f25943689e7ea14d867158c5a521eaf244e59d8e
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-05-08 15:21:00 +0530

    Remove lsp restart function

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,41 +8,20 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argc() == 0 then
-      vim.schedule(function()
-        vim.cmd("enew")
-        require("snacks").explorer()
-      end)
-    end
-  end,
+    callback = function()
+        if vim.fn.argc() == 0 then
+            vim.schedule(function()
+                vim.cmd("enew")
+                require("snacks").explorer()
+            end)
+        end
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-  callback = function()
-    if vim.bo.modified then
-      vim.cmd("silent write")
-    end
-  end,
-})
-
-vim.api.nvim_create_user_command("LspRestart", function()
-  vim.lsp.stop_client(vim.lsp.get_clients())
-  vim.cmd("edit")
-end, { desc = "Restart all LSP clients" })
-
-require("bufferline").setup({
-  options = {
-    show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    always_show_bufferline = true,
-    offsets = {
-      {
-        filetype = "snacks_layout_box",
-        text = "Explorer",
-        highlight = "Directory",
-        separator = true,
-      },
-    },
-  },
+    callback = function()
+        if vim.bo.modified then
+            vim.cmd("silent write")
+        end
+    end,
 })
