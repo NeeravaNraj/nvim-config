9409ba9d7d30686593b1a5ae4c0745e0ff35bbbb
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-05-29 15:22:00 +0530

    Keymap updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,17 +8,17 @@ vim.keymap.set("t", "<C-t>", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" }
 vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit insert mode (safe)" })
 
 vim.keymap.set("n", "<C-b>", function()
-  snacks.explorer()
+    snacks.explorer()
 end, { desc = "Toggle File Explorer" })
 
 vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, {
-  desc = "Show Diagnostics",
+    desc = "Show Diagnostics",
 })
 
 vim.keymap.set("n", "<leader>pf", function()
-  require("snacks").picker.files({
-    cwd = vim.fn.getcwd(),
-  })
+    require("snacks").picker.files({
+        cwd = vim.fn.getcwd(),
+    })
 end, { desc = "Find Files (cwd)" })
 
 -- vim.keymap.set("n", "<leader>rp", function()
@@ -26,22 +26,26 @@ end, { desc = "Find Files (cwd)" })
 -- end, { desc = "Recent Projects / Files" })
 
 vim.keymap.set("n", "<leader>wd", function()
-  require("snacks").picker.diagnostics()
+    require("snacks").picker.diagnostics()
 end, { desc = "Workspace Diagnostics" })
 
 vim.keymap.set("n", "<leader>pg", function()
-  require("snacks").picker.grep({ cwd = vim.fn.getcwd() })
+    require("snacks").picker.grep({ cwd = vim.fn.getcwd() })
 end, { desc = "Live Grep (cwd)" })
 
 vim.keymap.set("n", "<leader>rp", function()
-  require("snacks").picker.projects()
+    require("snacks").picker.projects()
 end, { desc = "Projects" })
 
 vim.keymap.set("n", "<leader>mp", function()
-  require("snacks").picker.man()
+    require("snacks").picker.man()
 end, { desc = "Man Pages" })
 
 vim.keymap.set("n", "<leader>f", "<cmd>Prettier<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+vim.keymap.set("n", "<leader>nh", function()
+    vim.o.hlsearch = not vim.o.hlsearch
+end, { desc = "Toggle search highlight" })
